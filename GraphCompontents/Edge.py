6b546ee64c37c1abfa7a547b6bdc21be1ee8d64e
Lorcan <lorcanjames1@gmail.com>
2024-03-28 13:30:58 +0000

    Add test for creation of test data and edge class

---
+++ GraphCompontents/Edge.py
@@ -0,0 +1,10 @@
+class Edge:
+    def __init__(self, edge_id: int, start: str, end: str, weight: int = 1, capacity: int = 1):
+        self.edge_ud = edge_id
+        self.start = start
+        self.end = end
+        self.weight = weight
+        self.capacity = capacity
+
+    def __str__(self):
+        print(f"Edge starts at {self.start} and ends at {self.end}, with a weight of {self.weight}")
---
+++ Tests/TestMockData.py
@@ -0,0 +1,55 @@
+import pytest
+from MockData.CreateGraphData import create_mock_data
+from GraphCompontents.Edge import Edge
+
+"""
+Know that data is of the below format so can manually check whether
+values in the df conform to the example shown below
+LinkId	Start	End	 Capacity	Weight
+1	    A	    B	 10	        5
+2	    B	    C	 10	        5
+3	    C	    D	 10	        5
+"""
+
+
+@pytest.fixture
+def edges():
+    e1 = Edge(1, 'A', 'B', 5, 10)
+    e2 = Edge(2, 'B', 'C', 5, 10)
+    e3 = Edge(3, 'C', 'D', 5, 10)
+    return [e1, e2, e3]
+
+
+@pytest.fixture
+def col_names():
+    return ["LinkId", "Start", "End", "Capacity", "Weight"]
+
+
+"""
+TODO: This might be too complicated for a test function
+"""
+
+
+def test_data_creation(edges, col_names):
+    df = create_mock_data()
+    for i, edge in enumerate(edges):
+        row = df.iloc[i]
+
+        for col_name in col_names[1:]:
+            match col_name:
+                case "Start":
+                    assert row["Start"] == edges[i].start
+                case "End":
+                    assert row["End"] == edges[i].end
+                case "Capacity":
+                    assert row["Capacity"] == edges[i].capacity
+                case "Weight":
+                    assert row["Weight"] == edges[i].weight
+                case _:
+                    raise Exception(f"Unknown column name found in Edge class. Col found was {col_name}")
+
+
+if __name__ == '__main__':
+    e = edges()
+    f = col_names()
+    test_data_creation(e, f)
