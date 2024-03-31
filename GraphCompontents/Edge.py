2354112e7dd78e30a0b05f595d7b8893c0ca8970
Lorcan <lorcanjames1@gmail.com>
2024-03-31 21:28:32 +0100

    remove concept of start and end from Edge class

--- GraphCompontents/Edge.py
+++ GraphCompontents/Edge.py
@@ -1,10 +1,14 @@
+from GraphCompontents.Node import Node
+
+
 class Edge:
-    def __init__(self, edge_id: int, start: str, end: str, weight: int = 1, capacity: int = 1):
+    def __init__(self, edge_id: int, node_1: Node, node_2: Node, weight: int = 1, capacity: int = 1, bi_directional=True):
         self.edge_id = edge_id
-        self.start = start
-        self.end = end
+        self.node_1 = node_1
+        self.node_2 = node_2
         self.weight = weight
         self.capacity = capacity
+        self.bi_directional = bi_directional
 
     def __str__(self):
-        print(f"Edge starts at {self.start} and ends at {self.end}, with a weight of {self.weight}")
+        print(f"Edge connects {self.node_1} to {self.node_2} with a weight of {self.weight}")
