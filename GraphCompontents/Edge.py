ae67ea3a84a553fdc910ce49bca0e21f8168ad3f
Lorcan <lorcanjames1@gmail.com>
2024-04-03 17:36:52 +0100

    simplify Edge class

--- GraphCompontents/Edge.py
+++ GraphCompontents/Edge.py
@@ -1,14 +1,12 @@
 from GraphCompontents.Node import Node
 
 
+# probably don't need an edge
 class Edge:
-    def __init__(self, edge_id: int, node_1: Node, node_2: Node, weight: int = 1, capacity: int = 1, bi_directional=True):
-        self.edge_id = edge_id
-        self.node_1 = node_1
-        self.node_2 = node_2
+    def __init__(self, weight: int = 1, capacity: int = 1, directed=False):
         self.weight = weight
         self.capacity = capacity
-        self.bi_directional = bi_directional
+        self.directed = directed
 
     def __str__(self):
         print(f"Edge connects {self.node_1} to {self.node_2} with a weight of {self.weight}")
